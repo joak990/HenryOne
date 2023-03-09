0cb8dba5a6e14d28ec71c1557a545f120893d98a
joak <joakhaidar@gmail.com>
2023-03-09 10:52:28 -0300

    module1

--- 05-EstructuraDeDatos-II/homework/homework.js
+++ 05-EstructuraDeDatos-II/homework/homework.js
@@ -10,10 +10,83 @@ Implementar la clase LinkedList, definiendo los siguientes métodos:
   search(isEven), donde isEven es una función que retorna true cuando recibe por parámetro un número par, busca un nodo cuyo valor sea un número par.
   En caso de que la búsqueda no arroje resultados, search debe retornar null.
 */
-function LinkedList() {}
+function LinkedList() {
+  this.head = null
+}
 
-function Node(value) {}
-
+function Node(value) {
+  this.value = value
+  this.next = null
+}
+LinkedList.prototype.search = function(param) {
+  if (this.head === null) {
+  return null;
+  }
+  let current = this.head;  
+  while (current !== null) {
+  if (typeof param === 'function') {
+  if (param(current.value)) {
+  return current.value;
+  }
+  } else if (typeof param === 'object') {
+  if (Object.is(current.value, param)) {
+  return current.value;
+  }
+  } else {
+  if (current.value === param) {
+  return current.value;
+  }
+  }
+  current = current.next;
+  }
+  return null;
+  };
+  
+  //metodo remover
+  LinkedList.prototype.remove = function (){
+   if ( this.head === null){
+   return null}
+   let current = this.head;
+   let last = null
+   if(current.next === null){
+    last = current 
+    this.head = null
+    return last.value
+   }else {
+   while (current.next.next !== null){
+   current = current.next
+  }
+  last = current.next
+  current.next = null
+  return last.value
+  }}
+  
+  //METODO ADD
+  LinkedList.prototype.add = function (value){                   
+    const newnode = new Node(value)
+    let current = this.head
+    //si no hay lista isertalo en head 
+  if(current === null){
+  this.head = newnode;
+  return value;
+  } 
+  while(current.next !==null){
+  current = current.next
+  }
+  current.next = newnode
+  }
+  
+  //instancia mi lista y nodos
+  const milista = new LinkedList()
+  
+  milista.add(4)
+  milista.add(3)
+  milista.add(4)
+  milista.add(4)
+  milista.add(4)
+  milista.add(4)
+  
+  
 /* EJERCICIO 2
 Implementar la clase HashTable.
 Nuetra tabla hash, internamente, consta de un arreglo de buckets (slots, contenedores, o casilleros; es decir, posiciones posibles para almacenar la información), donde guardaremos datos en formato clave-valor (por ejemplo, {instructora: 'Ani'}).
@@ -27,7 +100,49 @@ La clase debe tener los siguientes métodos:
 
 Ejemplo: supongamos que quiero guardar {instructora: 'Ani'} en la tabla. Primero puedo chequear, con hasKey, si ya hay algo en la tabla con el nombre 'instructora'; luego, invocando set('instructora', 'Ani'), se almacenará el par clave-valor en un bucket específico (determinado al hashear la clave)
 */
-function HashTable() {}
+function HashTable() {
+  
+  this.numBuckets = 35
+  this.buckets = [] 
+ }
+ 
+ HashTable.prototype.hash = function (string) {
+   let acumulador = 0;
+   for(const character of string){
+     const num = character.charCodeAt()
+     acumulador += num
+   }
+   return acumulador % this.numBuckets
+ }
+ 
+ 
+ HashTable.prototype.set = function (key,value){
+ if(typeof key !== "string") throw new TypeError("keys must be string");
+ const index = this.hash(key)
+ if(!this.buckets[index]){
+   this.buckets[index]={}
+ }
+ this.buckets[index][key]=value
+ }
+ 
+ HashTable.prototype.hasKey = function (key){
+   const index = this.hash(key);
+ return !!this.buckets[index][key] 
+ 
+ }
+ 
+ HashTable.prototype.get = function (key){
+   const index = this.hash(key);
+    return this.buckets[index][key];
+ }
+ 
+  let mihash = new HashTable()
+ 
+  console.log(mihash.hash("joaco"))
+  console.log(mihash.set("joaco",));
+ console.log(mihash.hasKey("joaco"));
+ 
+ console.log(mihash)
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
