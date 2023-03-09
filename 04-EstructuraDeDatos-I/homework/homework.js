07d84c68a7c43fdebf47f18cf6bd21061949a91c
joak <joakhaidar@gmail.com>
2023-03-09 10:49:23 -0300

    module1

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -14,9 +14,21 @@ Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
 Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera iterativa.
 */
 
-function nFactorial(n) {}
+function nFactorial(n) {
+  if (n === 0 )return 1
+else {
+ return n * nFactorial(n-1)
+}
+ 
+}
 
-function nFibonacci(n) {}
+function nFibonacci(n) {
+  if (n === 0 )return 0
+  if (n === 1 )return 1 
+  else{
+    return nFibonacci(n-1)+nFibonacci(n-2)
+  }
+}
 
 /*
 Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
@@ -27,7 +39,40 @@ Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde e
 Pueden utilizar class o función constructora.
 */
 
-function Queue() {}
+function Queue() {
+  this.users = [];
+  this.passwords = [];
+}
+Queue.prototype.addpass = function (value,...arg) {
+  if (Array.isArray(value)) {
+    value.forEach((el) => {
+      this.passwords.push(el);
+    });
+  } 
+  this.passwords.push(value)
+  
+  
+  arg.forEach((el)=> {
+    this.passwords.push(el)
+  })
+
+
+};
+Queue.prototype.enqueue = function (value) {
+  this.users.push(value);
+};
+Queue.prototype.dequeue = function () {
+  return this.passwords.shift();
+};
+Queue.prototype.size = function () {
+  return this.datos.length;
+};
+
+var Miq = new Queue();
+Miq.addpass("www","joaco");   
+
+console.log(Miq);
+
 
 /*⚠️ No modificar nada debajo de esta línea ⚠️*/
 module.exports = {
