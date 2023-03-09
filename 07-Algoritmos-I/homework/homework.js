0f5441234b850d904f6d4636b1f4ed8b57b2637d
joak <joakhaidar@gmail.com>
2023-03-09 10:55:03 -0300

    module1

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,8 +6,17 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
+  const arr =[1];               
+  let div = 2                     
+  while (num >1) {
+    if(num % div === 0){ arr.push(div)
+    num = num/div
+  }else { div++
+  }
+  }
+  return arr
+  }
 
-}
 
 function bubbleSort(array) {
   // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
@@ -15,7 +24,19 @@ function bubbleSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
-}
+  var n = array.length;
+    for (var i = 0; i < n - 1; i++) {
+      for (var j = 0; j < n - 1; j++) {
+        if (array[j] > array[j+1]) {
+          var temp = array[j];
+          array[j] = array[j+1];
+          array[j+1] = temp;
+        }
+      }
+    }
+    return array;
+  }
+
 
 
 function insertionSort(array) {
@@ -23,6 +44,17 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+
+  for (let j = 0; j < array.length; j++) {
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] < array[i - 1]) {
+        let aux = array[i];
+        array[i] = array[i - 1];
+        array[i - 1] = aux;
+
+      }
+    }
+  }return array
 
 }
 
